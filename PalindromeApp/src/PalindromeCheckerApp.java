4b1a299fd7e7769d905a2e5a5c7061286a70eb85
rs7787-ship-it <rs7787@srmist.edu.in>
2026-02-19 10:54:40 +0530

    [Rishi]addedUC1: Application Entry & Welcome Message(Updated)

--- PalindromeApp/src/PalindromeCheckerApp.java
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.print("Welcome To Palindrome Checker!");
-        System.out.print("Version: 1.0");
-        System.out.print("System Initialized Successfully");
+        System.out.println("Welcome To Palindrome Checker!");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialized Successfully");
     }
 }
