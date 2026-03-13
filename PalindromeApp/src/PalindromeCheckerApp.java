ab368849af8d2e5ab274735d9697bccc6c83dd15
rs7787-ship-it <rs7787@srmist.edu.in>
2026-03-13 08:20:18 +0530

    Update PalindromeCheckerApp.java

--- PalindromeApp/src/PalindromeCheckerApp.java
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -1,8 +1,23 @@
 import java.util.Scanner;
-public class PalindromeCheckerApp {
+public class UseCase2PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Palindrome Checker!");
-        System.out.println("Version: 1.0");
-        System.out.println("System Initialized Successfully");
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is Not a Palindrome");
+        }
     }
 }
